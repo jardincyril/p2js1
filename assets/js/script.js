1a4557adb4ef2c00ba3971ede0d8e858988d46a9
cyril jardin <cyilroc60@hotmail.fr>
2018-06-24 12:51:27 +0200

    p2js1

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Changement d'image quand la souris passe sur l'image de base, l'image de base revient quand on enlève le curseur de l'image.
+function ImgChange(img){
+  document.getElementById('Img').src = img;
+}
